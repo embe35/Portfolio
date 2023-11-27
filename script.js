8ee140981bc14689e666ad8e729ada6cd4efdfbd
embe35 <147669248+embe35@users.noreply.github.com>
2023-11-27 15:20:57 -0700

    Add alert button

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 console.log("hello world");
 document.getElementById("year")
 document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("btn-alert").addEventListener("click", function() {
+    alert("Hello to you too!");
+});
