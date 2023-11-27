b114b36ebf4e46e99efe793c46c28ea10adb8a80
embe35 <147669248+embe35@users.noreply.github.com>
2023-11-27 16:06:51 -0700

    update version

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 console.log("hello world");
 document.getElementById("year")
 document.getElementById("year").innerHTML = new Date().getFullYear();
-document.getElementById("btn-alert").addEventListener("click", function() {
+
+document.getElementById("btn-alert").onclick = () => {
     alert("Hello to you too!");
-});
+};
+
+document.getElementById('btn-alert').onmouseover = () => {
+    document.getElementById('btn-alert').innerText = '"Hello!"';
+};
+document.getElementById('btn-alert').onmouseleave = () => {
+    document.getElementById('btn-alert').innerText = "Say Hello!"
+};
+
